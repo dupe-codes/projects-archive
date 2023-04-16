09a2a4b22707ad72181a7fb41c3f649b6cb5b166
dupe <njdupe@gmail.com>
2023-04-16 17:14:16 -0400

    feat: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo 
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push public directory to gh-pages branch
+git subtree push --prefix public origin gh-pages
